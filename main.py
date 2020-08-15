1977f99a0bd2cb051355e657b63ae65fc74a9e44
Vinay <vinaykumarvk.8866@gmail.com>
2020-08-15 21:52:47 +0530

    Initialised main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+import time
+from queue import PriorityQueue
+
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+
+pygame.display.set_caption("A* Path Finding Algorithm")
+
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+VIOLET = (148, 0, 211) 
+INDIGO = (75, 0, 130)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 127, 0)
